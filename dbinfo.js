5d921c9df7bb1c4e9a124d172884edad7eca423a
Gundreddy <DKS0197020@dcsg.com>
2019-02-21 18:35:36 -0500

    dbinfo

---
+++ dbinfo.js
@@ -0,0 +1,12 @@
+const dboptions = {
+ 
+        user: 'nodeuser',
+        password: 'nodeuser',
+        database: 'cschat',
+        host: 'localhost',
+        connectionLimit: 10
+        
+        }
+
+
+module.exports=dboptions;
