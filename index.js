a33e60880b0639dc3099260793208d6958a8fcce
Gundreddy <DKS0197020@dcsg.com>
2019-03-08 11:27:15 -0500

    new db

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const server = express();
 const httpServer = require('http').Server(server);
 const io = require('socket.io')(httpServer);
-const mysqlcon = require('./sqlserv.js')();
+const mysqlcon = require('./mysqlcon.js')();
 server.use( express.static(__dirname));
 const conn =mysqlcon.pool;
 io.on("connection" , (socket) => {
